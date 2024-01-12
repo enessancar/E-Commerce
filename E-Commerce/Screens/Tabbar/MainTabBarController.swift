3117ae5c36d929e3484638f208bd2c28f46b36e8
enessancar <enes57751907@gmail.com>
2024-01-12 16:08:55 +0300

    tabbar , firestore service created, delegate

--- E-Commerce/Screens/Tabbar/MainTabBarController.swift
+++ E-Commerce/Screens/Tabbar/MainTabBarController.swift
@@ -5,4 +5,23 @@
 //  Created by Enes Sancar on 11.01.2024.
 //
 
-import Foundation
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    fileprivate func createNavController(for viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        navController.navigationBar.tintColor = .systemRed
+        return navController
+    }
+}
